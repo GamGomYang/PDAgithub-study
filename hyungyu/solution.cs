be4b4d77a4f6571742ff90d3da868a4cd9624eea
choihyungyu0 <zxcvasdfqwer9632@gmail.com>
2025-05-28 21:18:08 +0900

    fix.githun.c 수정

---
+++ hyungyu/solution.cs
@@ -0,0 +1,5 @@
+void main(){
+    println("Hello, GitHub!");
+    println("This is a simple C# program to demonstrate GitHub integration.");
+    println("Feel free to modify and explore the code.");
+}
